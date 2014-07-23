e14f77d88f4a0a398a1ba51b3d7842f35225726f
BillHHHuang <BillHuangg@gmail.com>
2014-07-23 22:38:33 +0800

    添加字体，导航
    导航位置不准确

--- js/custom.js
+++ js/custom.js
@@ -2,12 +2,17 @@
 jQuery(document).ready(function ($) {
 	
 	//
+	$(".navigationList").hide();
+
+
 	$(window).load(reset);
 	$(window).resize(reset);  
 
 	function reset(){
 		centreNavigation();
 		ResetBackgroudBox();
+
+		setTitleDisplayEffect();
 	}
 
 	// Background Background Image Reset
@@ -29,6 +34,40 @@ jQuery(document).ready(function ($) {
 		var windowWidth = $(window).width();
 		var navigatiionLeftPosition = windowWidth / 2 - navigationWidth / 2;
 		$(".navigationContainer").css("left",navigatiionLeftPosition + "px");
+	}
+
+	// TODO: 在第一次切换时，出现黑屏（加载）
+	// 		 需要具有判断目前正处于第几页的功能
+	$(".logoBox").mouseenter(function(){
+		//background image blur
+		$("#firstPage").css("background-image","url(Resources/firstPageBG_blur.png)");
+		$(".navigationContainer").addClass("circleBorder");
+		//$(".logoBox").css("margin","0");
+		$(".navigationList").show();
+
+	});
+
+	$(".navigationContainer").mouseleave(function(){
+		$("#firstPage").css("background-image","url(Resources/firstPageBG.png)");
+		// $(".navigationContainer").removeClass("circleBorder");
+		// $(".navigationList").hide();
+	});
+
+
+
+	// Part 1
+	function setTitleDisplayEffect(){
+		var titleWidth = 540;
+		var title = $(".title");
+		var windowWidth = $(window).width();
+		if (windowWidth < titleWidth) {
+			if (title.hasClass("titleLetterSpace")) {
+				title.removeClass("titleLetterSpace");
+			};
+		}
+		else if (windowWidth > titleWidth) {
+			title.addClass("titleLetterSpace");
+		};
 	}
 
 
