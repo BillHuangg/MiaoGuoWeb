923d67242283058e14c4d3ac50ba7da2b0eb8f11
BillHHHuang <BillHuangg@gmail.com>
2014-07-27 00:23:34 +0800

    主页面完成
    细节调整
    导航修改
    部分页面自适应

--- js/custom.js
+++ js/custom.js
@@ -12,7 +12,7 @@ jQuery(document).ready(function ($) {
 	function reset(){
 		
 		// 导航居中
-		centreNavigation();
+		//centreNavigation();
 
 		// 背景图高度响应浏览器高度
 		ResetBackgroudBox();
@@ -28,6 +28,11 @@ jQuery(document).ready(function ($) {
 
 		// Part 4 block
 		setBlockSize();
+
+		// TODO: Part 5
+
+		// Part 6
+		setWorksGroupBlockSize();
 	}
 
 	// Background Background Image Reset
@@ -41,6 +46,7 @@ jQuery(document).ready(function ($) {
 			windowHeight = 900;
 		}
 		$(".backgroundBox").css("min-height", windowHeight + "px");
+		$(".contentBox").css("margin-top", "-" + windowHeight + "px");
 	}
 
 
@@ -56,16 +62,20 @@ jQuery(document).ready(function ($) {
 	// 		 需要具有判断目前正处于第几页的功能
 	$(".logoBox").mouseenter(function(){
 		//background image blur
-		$("#firstPage").css("background-image","url(Resources/firstPageBG_blur.jpg)");
+		//$("#firstPage").css("background-image","url(Resources/firstPageBG_blur.jpg)");
 		$(".navigationContainer").addClass("circleBorder");
 		//$(".logoBox").css("margin","0");
 		$(".navigationList").show();
+
+		$("#firstPageBG").addClass("backgroundBlur");
 	});
 
 	$(".navigationContainer").mouseleave(function(){
-		$("#firstPage").css("background-image","url(Resources/firstPageBG.jpg)");
+		//$("#firstPage").css("background-image","url(Resources/firstPageBG.jpg)");
 		$(".navigationContainer").removeClass("circleBorder");
 		$(".navigationList").hide();
+		$("#firstPageBG").removeClass("backgroundBlur");
+
 	});
 
 
@@ -100,8 +110,10 @@ jQuery(document).ready(function ($) {
 
 	$(".iconContainer").mouseenter(function(){
 
-		$("#secondPage").css("background-image","url(Resources/secondPageBG_blur.jpg)");
-		
+		//$("#secondPage").css("background-image","url(Resources/secondPageBG_blur.jpg)");
+		$("#secondPageBG").addClass("backgroundBlur");
+		$("#secondPageBG").removeClass("outBlur");
+
 		// img
 		$(this).children("a").children(".clickedIcon").css("display","inline-block");
 		$(this).children("a").children(".unclickedIcon").css("display","none");
@@ -112,7 +124,9 @@ jQuery(document).ready(function ($) {
 
 	$(".iconContainer").mouseleave(function(){
 
-		$("#secondPage").css("background-image","url(Resources/secondPageBG.jpg)");
+		//$("#secondPage").css("background-image","url(Resources/secondPageBG.jpg)");
+		$("#secondPageBG").removeClass("backgroundBlur");
+		$("#secondPageBG").addClass("outBlur");
 
 		// img
 		$(this).children("a").children(".clickedIcon").css("display","none");
@@ -136,6 +150,18 @@ jQuery(document).ready(function ($) {
 			$(".members").css("width","800");
 		}
 	}
+	$(".memberContainer").mouseenter(function(){
+		$("#thirdPageBG").addClass("backgroundBlur");
+		$("#thirdPageBG").removeClass("outBlur");
+
+	});
+
+	$(".memberContainer").mouseleave(function(){
+		$("#thirdPageBG").removeClass("backgroundBlur");
+		$("#thirdPageBG").addClass("outBlur");
+
+	});
+
 
 	//********************************** Part 4 **********************************//
 	function setBlockSize(){
@@ -143,10 +169,11 @@ jQuery(document).ready(function ($) {
 		var limitation = 500;
 		if (windowWidth < limitation) {
 			$(".blockContainer").css("width", windowWidth);
+			$(".blockBGContainer").css("width", windowWidth);
 		}
 		else{
 			$(".blockContainer").css("width", windowWidth/3);
-
+			$(".blockBGContainer").css("width", windowWidth/3);
 		}
 		var windowHeight = $(window).height();
 		if (windowHeight < 770) {
@@ -156,9 +183,88 @@ jQuery(document).ready(function ($) {
 			windowHeight = 900;
 		}
 		$(".blockContainer").css("min-height", windowHeight + "px");
+		$(".blockBGContainer").css("min-height", windowHeight + "px");
 	}
 
+	$(".blockContainer").mouseenter(function(){
+		$(this).children(".blockBGContainer").addClass("backgroundBlur");
+		$(this).children(".blockBGContainer").removeClass("outBlur");
+
+		//$(this).css("z-index","0");
+	});
+
+	$(".blockContainer").mouseleave(function(){
+		$(this).children(".blockBGContainer").removeClass("backgroundBlur");
+		$(this).children(".blockBGContainer").addClass("outBlur");
+
+		//$(this).css("z-index","1");
+	});
+
 	//********************************** Part 5 **********************************//
+
+
+	//********************************** Part 6 **********************************//
+	function setWorksGroupBlockSize(){
+		var windowWidth = $(window).width();
+		var limitation = 584;
+		if (windowWidth < limitation) {
+			windowWidth = windowWidth;
+		}
+		else{
+			windowWidth = windowWidth / 4;
+		}
+
+		$(".workGroupContainer").css("width", parseInt(windowWidth));
+		$(".workContainer").css("width", parseInt(windowWidth));
+		$(".workBGContainer").css("width", parseInt(windowWidth));
+
+		var windowHeight = $(window).height();
+		if (windowHeight < 770) {
+			windowHeight = 770;
+		}
+		else if (windowHeight > 900) {
+			windowHeight = 900;
+		}
+
+		var partWidth = windowHeight / 4;
+
+		$(".workGroupContainer").css("min-height", windowHeight + "px");
+		$(".workContainer").css("min-height", partWidth + "px");
+		$(".workBGContainer").css("min-height", partWidth + "px");
+
+		var linePaddingLeft = (windowWidth - $(".workSubTitle").width()) / 2;
+		var linePaddingTop = (windowHeight/4 - $(".workSubTitle").height()) / 2;
+
+		$(".workSubTitle").css("padding-left", linePaddingLeft);
+		$(".workSubTitle").css("padding-right", linePaddingLeft);
+		$(".workSubTitle").css("padding-top", linePaddingTop + 6);
+		$(".workSubTitle").css("padding-bottom", linePaddingTop - 6);
+
+	}
+
+	$(".workContainer").mouseenter(function(){
+		if (!$(this).children(".workBGContainer").hasClass("noBlur")) {
+			$(this).children(".workBGContainer").addClass("backgroundBlur");
+			$(this).children(".workBGContainer").removeClass("outBlur");
+		}
+	});
+
+	$(".workContainer").mouseleave(function(){
+		if (!$(this).children(".workBGContainer").hasClass("noBlur")) {
+			$(this).children(".workBGContainer").removeClass("backgroundBlur");
+			$(this).children(".workBGContainer").addClass("outBlur");
+		}
+	});
+
+	$(".workGroupContainer").mouseenter(function(){
+		$(this).parent().children(".workGroupContainer").not(this).children(".blackMask").addClass("inBlackMask");
+	});
+
+	$(".workGroupContainer").mouseleave(function(){
+		$(this).parent().children(".workGroupContainer").not(this).children(".blackMask").removeClass("inBlackMask");
+	});
+
+	
 
 
 });
